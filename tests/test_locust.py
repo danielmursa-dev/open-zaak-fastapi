272a32c726de4fbf94970e11c81420b9f7e183c0
Daniel Mursa <daniel@maykinmedia.nl>
2025-07-14 10:11:51 +0200

    Improve tests

--- tests/test_locust.py
+++ tests/test_locust.py
@@ -4,4 +4,4 @@ from locust import HttpUser, task
 class GetList(HttpUser):
     @task
     def get(self):
-        self.client.get("/zaken/api/v1/zaken?pageSize=100")
+        self.client.get("/zaken/api/v1/zaken")
